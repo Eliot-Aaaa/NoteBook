455649c5582b8e5861152565b412541d0674ef98
unknown <wuzhijun@zlgmcu.com>
2020-07-28 11:04:11 +0800

    commit-7:为optionMenu添加图标，为备忘录内容编辑文本框添加文本监听，根据文本选择保存按钮是否可用

--- app/src/main/java/com/eliot/notebook/memo/MemoContentActivity.java
+++ app/src/main/java/com/eliot/notebook/memo/MemoContentActivity.java
@@ -1,6 +1,8 @@
 package com.eliot.notebook.memo;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -35,6 +37,11 @@ public class MemoContentActivity extends AppCompatActivity
     Memo memo;                                      //跳转到编辑界面时传入的对象
     int action;                                     //跳转进入编辑界面时是作为新增还是作为修改的判断
 
+    //文本的原始内容和修改内容，用于判定是否可用保存按钮
+    String originalContent;
+    String modifyContent;
+    boolean showingSaveButton;                         //是否正在显示保存按钮可用，true为正在显示可用，false为显示不可用
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -50,13 +57,21 @@ public class MemoContentActivity extends AppCompatActivity
             //如果能获取到对象，说明传入不为空，是作为修改传入的
             content_edit.setText(memo.getContent());
             action = Constants.MEMO_ACTION_UPDATE;
+            originalContent = memo.getContent();
+            modifyContent = originalContent;
         }
         else
         {
             //如果不能获取到对象，说明是作为新增传入的
             content_edit.setText("");
             action = Constants.MEMO_ACTION_NEW;
+            originalContent = "";
+            modifyContent = originalContent;
         }
+        showingSaveButton = false;                                  //初始默认不可点击保存按钮
+
+        //为EditText控件添加文本监视器，当文本发生变化时回调
+        content_edit.addTextChangedListener(contentTextWatcher);
 
         //设置Toolbar
         memo_content_tool_bar = findViewById(R.id.memo_content_tool_bar);
@@ -105,6 +120,41 @@ public class MemoContentActivity extends AppCompatActivity
         return true;
     }
 
+    //选项菜单的准备函数，使用invalidateOptionsMenu()调用此函数
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        //设置保存按钮的显示状态：可用或者不可用
+        menu.findItem(R.id.memo_content_save).setEnabled(showingSaveButton);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    //文本监视器
+    TextWatcher contentTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //将修改内容填入modifyContent，并且更新选项菜单
+            modifyContent = s.toString();
+            //当原始内容和修改内容相等时，说明没有进行修改，不可点击保存按钮；当修改内容为空，也就是编辑框内没有内容时，禁止使用保存按钮
+            boolean canShowSave = ! (originalContent.equals(modifyContent) || modifyContent.equals(""));
+            //当前保存按钮显示状态与检测到是否可显示冲突时，更新UI
+            if (showingSaveButton != canShowSave)
+            {
+                showingSaveButton = canShowSave;
+                invalidateOptionsMenu();
+            }
+        }
+    };
+
     private void saveContent()
     {
         if (dbManager == null)
@@ -121,6 +171,10 @@ public class MemoContentActivity extends AppCompatActivity
                 memo = memos.get(0);
             action = Constants.MEMO_ACTION_UPDATE;
             Toast.makeText(getApplicationContext(), "新增完成！", Toast.LENGTH_SHORT).show();
+            //新增文本内容后originalContent改变，并且重新刷新选项菜单
+            originalContent = content;
+            showingSaveButton = false;
+            invalidateOptionsMenu();
         }
     }
 
@@ -137,6 +191,10 @@ public class MemoContentActivity extends AppCompatActivity
             memo.setModifyTime(currentTime);
             dbManager.update(memo);
             Toast.makeText(getApplicationContext(), "修改完成！", Toast.LENGTH_SHORT).show();
+            //修改文本内容后originalContent改变，并且重新刷新选项菜单
+            originalContent = content;
+            showingSaveButton = false;
+            invalidateOptionsMenu();
         }
     }
 }
